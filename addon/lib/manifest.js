a72581449742241f58c1a5de8070eecbd4ab794e
David Young <davidy@funkypenguin.co.nz>
2024-02-01 18:46:14 +1300

    Add HTML  for a prettier landing page
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- addon/lib/manifest.js
+++ addon/lib/manifest.js
@@ -6,19 +6,22 @@ import { Type } from './types.js';
 
 const DefaultProviders = Providers.options.map(provider => provider.key);
 const CatalogMochs = Object.values(MochOptions).filter(moch => moch.catalog);
+const tenant_name = process.env.TENANT_NAME;
 
 export function manifest(config = {}) {
   const overrideManifest = getManifestOverride(config);
   const baseManifest = {
-    id: 'com.stremio.torrentio-sh.addon',
+    id: `com.elfhosted.${tenant_name}-torrentio.addon`,
     version: '0.0.14',
     name: getName(overrideManifest, config),
     description: getDescription(config),
+    descriptionHTML: getDescriptionHTML(config),
+    mochsHTML: getMochsHTML(config),
     catalogs: getCatalogs(config),
     resources: getResources(config),
     types: [Type.MOVIE, Type.SERIES, Type.ANIME, Type.OTHER],
-    background: `https://images6.alphacoders.com/134/1344105.png`,
-    logo: `https://i.ibb.co/w4BnkC9/GwxAcDV.png`,
+    background: `https://unsplash.com/photos/HTpAIzZRHvw/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzA2NjUxMTMxfA&force=true&w=1920`,
+    logo: `https://elfhosted.com/images/logo.svg`,
     behaviorHints: {
       configurable: true,
       configurationRequired: false
@@ -35,7 +38,7 @@ export function dummyManifest() {
 }
 
 function getName(manifest, config) {
-  const rootName = manifest?.name || 'Torrentio-sh';
+  const rootName = manifest?.name || `${tenant_name} | ElfHosted`;
   const mochSuffix = Object.values(MochOptions)
       .filter(moch => config[moch.key])
       .map(moch => moch.shortName)
@@ -54,9 +57,39 @@ function getDescription(config) {
       .join(' & ');
   const possibleMochs = Object.values(MochOptions).map(moch => moch.name).join('/')
   const mochsDesc = enabledMochs ? ` and ${enabledMochs} enabled` : '';
-  return 'Provides torrent streams from scraped torrent providers.'
+  return `${tenant_name}-torrentio provides torrent streams from scraped torrent providers.`
       + ` Currently supports ${enabledProvidersDesc}${mochsDesc}.`
-      + ` To configure providers, ${possibleMochs} support and other settings visit https://torrentio.strem.fun`
+      + ` To configure providers, ${possibleMochs} support and other settings visit https://${tenant_name}-torrentio.elfhosted.com`
+}
+
+function getDescriptionHTML(config) {
+  const providersList = config[Providers.key] || DefaultProviders;
+  const enabledProvidersDesc = Providers.options
+      .map(provider => `${provider.label}${providersList.includes(provider.key) ? '(+)' : '(-)'}`)
+      .join(' / ')
+  const enabledMochs = Object.values(MochOptions)
+      .filter(moch => config[moch.key])
+      .map(moch => moch.name)
+      .join(' & ');
+  const possibleMochs = Object.values(MochOptions).map(moch => moch.name).join(' / ')
+  const mochsDesc = enabledMochs ? ` and ${enabledMochs} enabled` : '';
+  return `This is an open-source, hacky <A HREf="https://github.com/geek-cookbook/torrentio.elfhosted.com">fork</A> of <A HREF="http://torrentio.strem.fm">torrent.strem.fm</A>, implemented by <A HREF="https://elfhosted.com">ElfHosted</A>, which provides torrent streams from scraped torrent providers.`
+      + ` <br/><br/>(<I>ElfHosted is a <A HREF="https://elfhosted.com/open/">open-source PaaS</A> built and run by <A HREF="https://geek-cookbook.funkypenguin.co.nz/community/">geeks</A></I>)`
+      + ` <br/><br/>The primary instance (<A HREF="http://torrentio.elfhosted.com">torrentio.elfhosted.com</A>) is provided free for public use, and <A HREF="https://github.com/funkypenguin/elf-infra/blob/ci/torrentio/middleware-rate-limit-torrentio.yaml">rate-limited</A> appropriately for casual individual streaming use (<I>not automation</I>).`
+      + ` <br/><br/>Hosted instances with <A HREF="https://github.com/geek-cookbook/elf-charts/blob/main/charts/other/myprecious/templates/middleware/middleware-torrentio-rate-limit.yaml">rate-limits</A> appropriate for automation <A HREF="https://elfhosted.com/guides/media/stream-from-real-debrid-with-self-hosted-torrentio/">are available</A> for $0.15/day, with $10 free credit on new accounts.`
+      + ` <br/><br/>An internal, un-rate-limited instance is provided free, with all <A HREF="https://elfhosted.com/apps/">ElfHosted apps</A>, for automation.`
+      + ` <br/><br/>Indexers currently supported are ${enabledProvidersDesc}${mochsDesc}.`
+      + ` <br/><br/>Providers currently supported are ${possibleMochs}`
+}
+
+function getMochsHTML(config) {
+  const enabledMochs = Object.values(MochOptions)
+      .filter(moch => config[moch.key])
+      .map(moch => moch.name)
+      .join(' & ');
+  const possibleMochs = Object.values(MochOptions).map(moch => moch.name).join(' / ')
+  const mochsDesc = enabledMochs ? ` and ${enabledMochs} enabled` : '';
+  return `Supported providers are currently: <ul>${possibleMochs}</ul>`
 }
 
 function getCatalogs(config) {
